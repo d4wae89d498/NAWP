921931c98a3fa5d4120379a469eaaa2d529f4974
Faussurier Marc <faussurier.marc@icloud.com>
2018-09-14 21:41:06 +0200

    [wip] finished clientSideRendering class draft

--- src/tsclient/clientSideRendering.ts
+++ src/tsclient/clientSideRendering.ts
@@ -1,32 +1,44 @@
 import * as $ from "jquery";
+import Twig from "twig";
 
 /**
  * The client side rendering class
  */
 export class ClientSideRendering {
+
     /**
      * Will render a template using its data-id, will append or replace using the given id.
      * @param {string} templateDataId
      * @param {object} states
      */
     public static render(templateDataId: string, states: object): void {
+        // if this template id is already in memory
         if (typeof (window["templates"][templateDataId]) !== "undefined") {
-            $("[data-item-id=\"" + templateDataId + "\"]").after("");
-            // re-render window["templates"][template] with states
-        } else {
+            // we re-render it using twig.js and jquery
+            const template = Twig.twig({
+                data: window["baseTemplates"][this.IdToType(templateDataId)]
+            });
+            let output: string = template.render(states);
+            $("[data-item-id=\"" + this.getMaxType(templateDataId) + "\"]").html(output);
+        }
+        // else, we try to append this template
+        else {
             const nonDigit: string = this.IdToType(templateDataId);
             let maxId: number = 0;
             for (let tpl in window["templates"]) {
                 if (window["templates"].hasOwnProperty(tpl)) {
                     if (tpl.substr(0, tpl.length - (tpl.length - nonDigit.length)) === nonDigit) {
-                        let digit: number = parseInt(tpl.replace(/\D/g, ""));
                         maxId++;
                     }
                 }
             }
             if (maxId > 0) {
                 // append here
-                $("[data-item-id=\"" + this.getMaxType(templateDataId) + "\"]").after("");
+                const template = Twig.twig({
+                    data: window["baseTemplates"][this.IdToType(templateDataId)]
+                });
+                let output: string = template.render(states);
+                $("[data-item-id=\"" + this.getMaxType(templateDataId) + "\"]").after(output);
             }
         }
         return;
