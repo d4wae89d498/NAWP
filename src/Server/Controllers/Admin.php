d2b384a9846ab5e49ce538229ea53c4539e73c95
Marc Faussurier <faussurier.marc@icloud.com>
2018-11-15 21:15:07 +0100

    admin controller indentation

--- src/Server/Controllers/Admin.php
+++ src/Server/Controllers/Admin.php
@@ -87,9 +87,9 @@ class Admin extends Controller implements ControllerInterface
             foreach ($loginFields as $k => $v):
                 if (isset($_POST["accessTypeRadio"]) && !(($_POST["accessTypeRadio"] == "login") and in_array($k, $registrationFields))) :
                     $loginFields[$k]["v"] = $fieldToError($k) === null ? $_POST[$k] : "";
-        $loginFields[$k]["m"] = $fieldToError($k);
-        endif;
-        endforeach;
+                    $loginFields[$k]["m"] = $fieldToError($k);
+                endif;
+            endforeach;
         endif;
         /**
          * return true if we can proceed the form
@@ -109,30 +109,32 @@ class Admin extends Controller implements ControllerInterface
             if ($allFieldsAreCorrect()) :
                 switch ($_POST["accessTypeRadio"]):
                     case "register":
-                            $loginMessage = "IN REGISTER WITH VALID POSTS";
-        break;
-        case "login":
+                                $loginMessage = "IN REGISTER WITH VALID POSTS";
+                        break;
+                    case "login":
                         $userRecord = $atlas
                         ->select(User::class)
                         ->where('email = ', $_POST["firstName"])
                         ->fetchRecord();
-        if (($userRecord === null) || ($userRecord->hashed_password !== Utils::hashPassword($_POST["pin"]))) :
+                        if (($userRecord === null) || ($userRecord->hashed_password !== Utils::hashPassword($_POST["pin"]))) :
                             // wrong email and/or password
                             $loginMessage = "Mot de passe ou utilisateur incorect (" . sha1($_POST["pin"] . $_ENV["PASSWORD_SALT"]).")"; else:
                             $_GET["UID"] = $uid = Utils::generateUID(9);
-        $url = "/admin";
-        if ($viewLogger->cookiePoolInstance->areCookieEnabled()):
-                                $viewLogger->cookiePoolInstance->set(new Cookie("UID", $uid)); else:
+                            $url = "/admin";
+                            if ($viewLogger->cookiePoolInstance->areCookieEnabled()):
+                                $viewLogger->cookiePoolInstance->set(new Cookie("UID", $uid));
+                            else:
                                 $url = Utils::buildUrlParams($url, ["UID" => $uid]);
-        endif;
-        $viewLogger->sessionInstance->set("user_id", 5);
-        $viewLogger->redirectTo($httpResponse, $url, $args);
-        return true;
-        endif;
-        break;
-        endswitch; else:
+                            endif;
+                            $viewLogger->sessionInstance->set("user_id", 5);
+                            $viewLogger->redirectTo($httpResponse, $url, $args);
+                            return true;
+                        endif;
+                    break;
+                endswitch;
+            else:
                 $loginMessage = "Please fill incorrect fields";
-        endif;
+            endif;
         endif;
         // rendering the page
         $newBody = $viewLogger->kernel->factories->getStreamFactory()->createStream();
